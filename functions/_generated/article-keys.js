7a0b9e1eae27a847cbb63bbac171469a60f55456
JacobShin0601 <yunchae@umich.edu>
2026-09-19 19:29:51 +0900

    content: add Korean CFA learning essay with accessible diagrams

--- functions/_generated/article-keys.js
+++ functions/_generated/article-keys.js
@@ -2,5 +2,6 @@ export const ARTICLE_KEYS = [
   "multi-objective-optimization-battery-supply-chain",
   "pareto-optimization-dinner-with-friends",
   "what-is-optimization-dinner-with-friends",
+  "why-i-started-cfa-as-a-data-scientist",
   "why-this-archive-is-open"
 ];
